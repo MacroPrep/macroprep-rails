5dd34364ab9a1a3dbc0796b6a5281a2127ea7a36
Jake LaCombe <jake.lacombe2@gmail.com>
2017-06-10 19:27:31 -0500

    Create pages for signing in and out users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'home#index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new'
+    get 'sign_up', to: 'users/registrations#new'
+    delete 'sign_out', to: 'users/sessions#destroy', as: 'user_sign_out'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
